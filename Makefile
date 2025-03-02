cf31d9c42125264012d824126e582270533ea451
guicdas <silva.caldeira.guilherme@gmail.com>
2025-03-02 19:49:56 +0000

    mariadb just missing .env access

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
 all: build
 
-build:	c
-	docker compose up --build #--no-cache
+build:	clr
+	docker compose up --build --no-cache
 
-clean: c
-	docker compose down
+clean: clr
+	docker compose -f srcs/docker-compose.yml down
 
 fclean:	clean
-	docker image rm nginx
-	docker container prune
+	docker system prune --all --force
+	docker container prune --force
+	docker builder prune --all --force
+	docker volume prune --all --force
+	docker image prune --all --force
+	#rm -rf /home//data/wp-files/ /home//data/wp-db/ /home//data/website-files/
 
-c:
+maria: clr
+	docker compose -f srcs/docker-compose.yml up -d --build 'mariadb'
+	docker run -it mariadb
+
+clr:
 	clear
--- srcs/requirements/mariadb/tools/mdb_users_config.sh
+++ srcs/requirements/mariadb/tools/mdb_users_config.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+
 service mariadb start
 
-mysql -e "CREATE USER IF NOT EXISTS '${DB_NAME}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+mysql -e "CREATE USER IF NOT EXISTS `${DB_NAME}`;"
 mysql -e "CREATE USER IF NOT EXISTS '${MDB_USER}'@'localhost' IDENTIFIED BY '${MDB_PASS}';"
-# '%'
-mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASS}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${MDB_PASS};"
+
+
+#mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASS}';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${MDB_PASS};"
 # -u root -p ${SQL_ROOT_PASSWORD}
-mysql -e "FLUSH PRIVILEGES;"
+#mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p ${SQL_ROOT_PASSWORD} shutdown
+#mysqladmin -u root -p ${SQL_ROOT_PASSWORD} shutdown
 
-exec mysqld_safe
+#exec mysqld_safe
 # "$@"
-#sem mysql??
